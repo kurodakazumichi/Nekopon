eb6b93968e0f7080986475aef3d91191c4a5a490
kazumichi kuroda <kazumichi96@gmail.com>
2021-02-21 23:04:35 +0900

    Added ResourceManager

---
+++ Assets/MyGame/Scripts/Manager/ResourceManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.AddressableAssets;
+
+namespace MyGame
+{
+  public class ResourceManager : SingletonMonobehaviour<ResourceManager>
+  {
+    /// <summary>
+    /// リソースの非同期ロードを行う
+    /// </summary>
+    /// <typeparam name="T">ロードするリソースの種類</typeparam>
+    /// <param name="address">Addressable Asssetsで登録したAddress</param>
+    /// <param name="pre">リソースロード前に呼ばれるコールバック関数</param>
+    /// <param name="post">ロード完了したリソースを受け取る関数</param>
+    /// <param name="done">ロード完了時に呼ばれる関数</param>
+    public void Load<T>(string address, Action pre, Action<T> post, Action done)
+    {
+      pre();
+      Addressables.LoadAssetAsync<T>(address).Completed += op => {
+        post(op.Result);
+        done();
+      };
+    }
+  }
+}
