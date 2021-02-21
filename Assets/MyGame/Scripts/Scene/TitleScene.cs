b8b6c3b4b1d6529fc518c84be5e91b220f640949
kazumichi kuroda <kazumichi96@gmail.com>
2021-02-21 18:41:56 +0900

    Added TitleScene

---
+++ Assets/MyGame/Scripts/Scene/TitleScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace MyGame.Scene
+{
+  public class TitleScene : SceneBase
+  {
+    private GameObject rTitleLogo = null;
+
+    protected override void MyStart()
+    {
+      Addressables.LoadAssetAsync<GameObject>("TitleLogo").Completed += op => { this.rTitleLogo = op.Result; };
+    }
+
+    private bool isCreated = false;
+    protected override void MyUpdate()
+    {
+      if (this.rTitleLogo != null && this.isCreated == false) {
+        Instantiate(this.rTitleLogo);
+        this.isCreated = true;
+
+      }
+    }
+  }
+}
