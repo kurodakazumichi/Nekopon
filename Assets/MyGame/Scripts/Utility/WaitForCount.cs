592048b87dd8673905c1cefd9a14b58445fd3bee
kazumichi kuroda <kazumichi96@gmail.com>
2021-02-22 21:08:50 +0900

    Added WaitForCount

---
+++ Assets/MyGame/Scripts/Utility/WaitForCount.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyGame
+{
+  /// <summary>
+  /// カスタムコルーチン、カウンターが0になるまで継続する
+  /// </summary>
+  class WaitForCount : CustomYieldInstruction
+  {
+    private int count = 0;
+
+    public void inc() { ++this.count; }
+    public void dec() { --this.count; }
+    public WaitForCount() { this.count = 0; }
+
+    public override bool keepWaiting {
+      get {
+        return (0 < this.count);
+      }
+    }
+  }
+}
