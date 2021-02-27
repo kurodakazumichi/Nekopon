919feebfec3a814c983d04f6822b2c28639909c2
kazumichi kuroda <kazumichi96@gmail.com>
2021-02-28 00:05:25 +0900

    Added InputManager, GamePad

---
+++ Assets/MyGame/Scripts/Manager/InputManager/GamePad.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace MyGame.InputManagement
+{
+
+  public class GamePad
+  {
+    /// <summary>
+    /// パッドに存在する軸のテーブル
+    /// </summary>
+    private Dictionary<AxisType, Axis> axes = new Dictionary<AxisType, Axis>();
+
+    /// <summary>
+    /// パッドに存在するボタンのテーブル
+    /// </summary>
+    private Dictionary<ButtonType, Button> buttons = new Dictionary<ButtonType, Button>();
+
+    public bool IsConnectedPad { get; private set; } = false;
+
+
+
+    public GamePad(int padNo)
+    {
+      // 接続されているパッドがあるかどうかのフラグ
+      IsConnectedPad = padNo <= (Input.GetJoystickNames().Length - 1);
+
+      // 軸のセットアップ
+      this
+        .SetupAxis(padNo, AxisType.AxisLX  , 1 , false)
+        .SetupAxis(padNo, AxisType.AxisLY  , 2 , true)
+        .SetupAxis(padNo, AxisType.AxisRX  , 4 , false)
+        .SetupAxis(padNo, AxisType.AxisRY  , 5 , true)
+        .SetupAxis(padNo, AxisType.AxisDX  , 6 , false)
+        .SetupAxis(padNo, AxisType.AxisDY  , 7 , false)
+        .SetupAxis(padNo, AxisType.Triggers, 3 , false)
+        .SetupAxis(padNo, AxisType.LT      , 9 , false)
+        .SetupAxis(padNo, AxisType.RT      , 10, false);
+
+      // ボタンのセットアップ
+      this
+        .SetupButton(padNo, ButtonType.A, 0)
+        .SetupButton(padNo, ButtonType.B, 1)
+        .SetupButton(padNo, ButtonType.X, 2)
+        .SetupButton(padNo, ButtonType.Y, 3)
+        .SetupButton(padNo, ButtonType.L1, 4)
+        .SetupButton(padNo, ButtonType.R1, 5)
+        .SetupButton(padNo, ButtonType.LS, 8)
+        .SetupButton(padNo, ButtonType.RS, 9)
+        .SetupButton(padNo, ButtonType.Back, 6)
+        .SetupButton(padNo, ButtonType.Start, 7);
+    }
+
+    public void Update()
+    {
+      Util.ForEach<AxisType>((value) => { this.axes[value].Update(); });
+      Util.ForEach<ButtonType>((value) => {  this.buttons[value].Update(); });
+    }
+
+    private GamePad SetupAxis(int padNo, AxisType type, int no, bool invert)
+    {
+      this.axes[type] = new Axis(type, $"Joy{padNo + 1}_Axis{no}", invert);
+      return this;
+    }
+
+    private GamePad SetupButton(int padNo, ButtonType type, int no)
+    {
+      this.buttons[type] = new Button(type, $"Joy{padNo + 1}_Button{no}");
+      return this;
+    }
+    
+#if _DEBUG
+    public void OnGUIDebug()
+    {
+      using (new GUILayout.VerticalScope(GUI.skin.box)) {
+        Util.ForEach<AxisType>((value) => { 
+          this.axes[value].OnGUIDebug();
+        });
+      }
+
+      using (new GUILayout.VerticalScope(GUI.skin.box)) {
+        Util.ForEach<ButtonType>((value) => {
+          this.buttons[value].OnGUIDebug();
+        });
+      }
+    }
+#endif
+  }
+
+}
---
+++ Assets/MyGame/Scripts/Manager/InputManager/InputManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyGame.InputManagement;
+
+namespace MyGame
+{
+  
+  public class InputManager : SingletonMonobehaviour<InputManager>
+  {
+    private GamePad[] pads = new GamePad[2];
+
+    protected override void MyStart()
+    {
+      this.pads[0] = new GamePad(0);
+      //this.pads[1] = new GamePad(1);
+    }
+
+    
+    protected override void MyUpdate()
+    {
+      this.pads[0].Update();
+      for(int i = 0; i < this.pads.Length; ++i) {
+        //this.pads[i].Update();
+      }
+    }
+
+#if _DEBUG
+    private void OnGUI()
+    {
+      this.pads[0].OnGUIDebug();
+    }
+#endif
+  }
+
+}
