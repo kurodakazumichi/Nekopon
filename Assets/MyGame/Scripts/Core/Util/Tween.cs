c548657f4851463609436304373ebb46ac22c038
kazumichi kuroda <kazumichi96@gmail.com>
2021-03-04 01:58:52 +0900

    Updated Tween

--- Assets/MyGame/Scripts/Core/Util/Tween.cs
+++ Assets/MyGame/Scripts/Core/Util/Tween.cs
@@ -6,6 +6,43 @@ namespace MyGame
 {
   public static class Tween
   {
+    /// <summary>
+    /// Tweenのタイプ
+    /// </summary>
+    public enum Type
+    {
+      EaseInSine,
+      EaseOutSine,
+      EaseInOutSine,
+      EaseOutBounce,
+    }
+
+    public static float easing(Type type, float t)
+    {
+      switch(type) {
+        case Type.EaseInSine: return EaseInSine(t);
+        case Type.EaseOutSine: return EaseOutSine(t);
+        case Type.EaseInOutSine: return EaseInOutSine(t);
+        case Type.EaseOutBounce: return EaseOutBounce(t);
+        default: return EaseInSine(t);
+      }
+    }
+
+    public static float EaseInSine(float t)
+    {
+      return 1 - Mathf.Cos((t * Mathf.PI) / 2);
+    }
+
+    public static float EaseOutSine(float t)
+    {
+      return Mathf.Sin((t * Mathf.PI) / 2);
+    }
+
+    public static float EaseInOutSine(float t)
+    {
+      return -(Mathf.Cos(Mathf.PI * t) - 1) / 2;
+    }
+
     public static float EaseOutBounce(float t)
     {
       const float n1 = 7.5625f;
