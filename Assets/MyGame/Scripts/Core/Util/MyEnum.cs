812f244e3e6570868cca34df33b71b69489481e9
kazumichi kuroda <kazumichi96@gmail.com>
2021-03-04 06:08:23 +0900

    Added MyEnum Util

---
+++ Assets/MyGame/Scripts/Core/Util/MyEnum.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace MyGame
+{
+  public static class MyEnum
+  {
+    /// <summary>
+    /// Enumの中からランダムな値を取得
+    /// </summary>
+    public static T Random<T>() where T : Enum
+    {
+      var count = Enum.GetNames(typeof(T)).Length;
+      var index = UnityEngine.Random.Range(0, count);
+      var value = Enum.GetValues(typeof(T)).GetValue(index);
+      return (T)value;
+    }
+  }
+}
