53a5c459bf9c0439dad92394ccb23544fa157bd5
kazumichi kuroda <kazumichi96@gmail.com>
2021-03-04 02:10:13 +0900

    Comment

--- Assets/MyGame/Scripts/Core/Util/MyMath.cs
+++ Assets/MyGame/Scripts/Core/Util/MyMath.cs
@@ -5,6 +5,9 @@ namespace MyGame
 
   public static class MyMath
   {
+    /// <summary>
+    /// 小数点の桁数を指定して丸める
+    /// </summary>
     public static float Round(float num, int digit)
     {
       float pow = Mathf.Pow(10, digit);
