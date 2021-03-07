41c3a8766d86b6d582e0403e39c8792dd839fc54
kazumichi kuroda <kazumichi96@gmail.com>
2021-03-08 06:02:58 +0900

    Added セーブ関連

---
+++ Assets/MyGame/Scripts/Manager/SaveManager/PlayerConfig.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using MyGame.Define;
+namespace MyGame.SaveManagement
+{
+  /**
+   * プレイヤーのHPやMPなどのステータスに関する設定
+   */
+  [CreateAssetMenu(menuName ="MyGame/Create/PlayerConfig")]
+  public class PlayerConfig : ScriptableObject
+  {
+    /// <summary>
+    /// 最大HP
+    /// </summary>
+    public int MaxHp = 0;
+    
+    /// <summary>
+    /// 最大MP
+    /// </summary>
+    [SerializeField]
+    private List<int> maxMp = new List<int>(App.AttributeCount);
+
+    /// <summary>
+    /// 使用MP(スキル使用に必要なMPの設定)
+    /// </summary>
+    [SerializeField]
+    private List<int> useMp = new List<int>(App.AttributeCount);
+
+    /// <summary>
+    /// 右クリックメニューから生成されたときに一度動作する
+    /// </summary>
+    private void Awake()
+    {
+      MyEnum.ForEach<App.Attribute>((attribute) => { 
+        this.maxMp.Add(0);
+        this.useMp.Add(0);
+      });
+    }
+
+    public int GetMaxMp(App.Attribute attribute)
+    {
+      return this.maxMp[(int)attribute];
+    }
+    public void SetMaxMp(App.Attribute attribute, int mp)
+    {
+      this.maxMp[(int)attribute] = mp;
+    }
+
+    public int GetUseMp(App.Attribute attribute)
+    {
+      return this.useMp[(int)attribute];
+    }
+    public void SetUseMp(App.Attribute attribute, int mp)
+    {
+      this.useMp[(int)attribute] = mp;
+    }
+
+  }
+
+#if UNITY_EDITOR
+
+  /// <summary>
+  /// PlayerConfigのInspector拡張
+  /// </summary>
+  [CustomEditor(typeof(PlayerConfig))]
+  public class PlayerConfigEditor : Editor 
+  {
+    private PlayerConfig config;
+
+    public override void OnInspectorGUI()
+    {
+      config = target as PlayerConfig;
+
+      // 最大HP
+      config.MaxHp = EditorGUILayout.IntField("最大HP", config.MaxHp);
+
+      // 最大MP(属性ごと)
+      EditorGUILayout.LabelField("最大MP");
+      MaxMpField("火", App.Attribute.Fir);
+      MaxMpField("水", App.Attribute.Wat);
+      MaxMpField("雷", App.Attribute.Thu);
+      MaxMpField("氷", App.Attribute.Ice);
+      MaxMpField("木", App.Attribute.Tre);
+      MaxMpField("聖", App.Attribute.Hol);
+      MaxMpField("闇", App.Attribute.Dar);
+
+      EditorGUILayout.LabelField("使用MP(スキル使用に必要なMPの設定)");
+      UseMpField("火", App.Attribute.Fir);
+      UseMpField("水", App.Attribute.Wat);
+      UseMpField("雷", App.Attribute.Thu);
+      UseMpField("氷", App.Attribute.Ice);
+      UseMpField("木", App.Attribute.Tre);
+      UseMpField("聖", App.Attribute.Hol);
+      UseMpField("闇", App.Attribute.Dar);
+
+      if (GUILayout.Button("Test")) {
+        Debug.Logger.Log(UnityEngine.JsonUtility.ToJson(config));
+      }
+    }
+
+    /// <summary>
+    /// 最大MP
+    /// </summary>
+    private void MaxMpField(string name, App.Attribute attribute)
+    {
+      int mp = EditorGUILayout.IntField(name, config.GetMaxMp(attribute));
+      config.SetMaxMp(attribute, mp);
+    }
+
+    /// <summary>
+    /// 使用MP
+    /// </summary>
+    private void UseMpField(string name, App.Attribute attribute)
+    {
+      int mp = EditorGUILayout.IntField(name, config.GetUseMp(attribute));
+      config.SetUseMp(attribute, mp);
+    }
+  }
+#endif
+
+}
---
+++ Assets/MyGame/Scripts/Manager/VersusManager/Player.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyGame.VersusManagement
+{
+  /// <summary>
+  /// 対戦プレイヤーに該当するクラス
+  /// </summary>
+  public class Player
+  {
+    //-------------------------------------------------------------------------
+    // プレイヤーステータスクラス
+    public class Status
+    {
+      /// <summary>
+      /// HP
+      /// </summary>
+      public LimitedFloat HP = new LimitedFloat();
+
+      /// <summary>
+      /// MP
+      /// </summary>
+      private Dictionary<Define.App.Attribute, LimitedFloat> mp = new Dictionary<Define.App.Attribute, LimitedFloat>();
+
+      /// <summary>
+      /// コンストラクタ
+      /// </summary>
+      public Status()
+      {
+        MyEnum.ForEach<Define.App.Attribute>((attribute) => { 
+          this.mp.Add(attribute, new LimitedFloat());
+        });
+      }
+#if _DEBUG
+      public void OnDebug()
+      {
+        using (new GUILayout.VerticalScope()) {
+          GUILayout.Label($"HP:{HP.Now}");
+          Util.ForEach(this.mp, (attr, mp) => { 
+            GUILayout.Label($"MP_{attr}:{mp.Now}");
+          });
+        }
+      }
+#endif
+    }
+
+    //-------------------------------------------------------------------------
+    // メンバ変数
+
+    /// <summary>
+    /// 親オブジェクト
+    /// </summary>
+    private Transform parent = null;
+
+    /// <summary>
+    /// Playerに関するゲームオブジェクトを格納しておくゲームオブジェクト
+    /// </summary>
+    private Transform folder = null;
+
+    /// <summary>
+    /// ロケーション情報
+    /// </summary>
+    private Location location;
+
+    /// <summary>
+    /// ステータス
+    /// </summary>
+    private Status status = null;
+
+    /// <summary>
+    /// パズル
+    /// </summary>
+    private Puzzle puzzle = null;
+
+    //-------------------------------------------------------------------------
+    // Load, Unload
+
+    public static void Load(System.Action pre, System.Action done)
+    {
+      Puzzle.Load(pre, done);
+    }
+
+    public static void Unload()
+    {
+      Puzzle.Unload();
+    }
+
+    //-------------------------------------------------------------------------
+    // 生成・準備系
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public Player(Transform parent, Location location)
+    {
+      this.parent   = parent;
+      this.location = location;
+      this.status   = new Status();
+    }
+
+    /// <summary>
+    /// 各種オブジェクトの生成、初期化
+    /// </summary>
+    public void Init()
+    {
+      // プレースフォルダーを作成
+      this.folder = new GameObject("Player").transform;
+      this.folder.parent = this.parent;
+
+      // パズルを作成
+      this.puzzle = new Puzzle(this.folder, this.location.Paw);
+      this.puzzle.Init();
+    }
+
+    /// <summary>
+    /// プレイヤーのセットアップ
+    /// </summary>
+    public void Setup()
+    {
+      this.puzzle.Setup();
+    }
+
+    public void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Alpha2)) {
+        this.puzzle.ShowCursor();
+      }
+      if (Input.GetKeyDown(KeyCode.Alpha3)) {
+        this.puzzle.HideCursor();
+      }
+
+      if (Input.GetKeyDown(KeyCode.D)) {
+        Debug.Logger.Log(this.puzzle.HasMovingPaw);
+      }
+
+      // カーソル移動(上下左右)
+      if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+        this.puzzle.MoveCursorL();
+      }
+      if (Input.GetKeyDown(KeyCode.RightArrow)) {
+        this.puzzle.MoveCursorR();
+      }
+      if (Input.GetKeyDown(KeyCode.UpArrow)) {
+        this.puzzle.MoveCursorU();
+      }
+      if (Input.GetKeyDown(KeyCode.DownArrow)) {
+        this.puzzle.MoveCursorD();
+      }
+
+      // 肉球選択 or 入れ替え
+      if (Input.GetKeyDown(KeyCode.Z)) {
+        // 選択中の肉球があれば入れ替え
+        if (this.puzzle.HasSelectedPaw) {
+          this.puzzle.Swap();
+        }
+
+        // 選択中の肉中がなければカーソルのある所の肉球を選択
+        else {
+          this.puzzle.SelectPaw();
+        }
+      }
+
+      // 肉球選択の解除
+      if (Input.GetKeyDown(KeyCode.X)) {
+        this.puzzle.ReleasePaw();
+      }
+
+      // 連鎖
+      if (Input.GetKeyDown(KeyCode.S)) {
+        this.puzzle.StartChain();
+      }
+      if (this.puzzle != null && this.puzzle.IsInChain) {
+        this.puzzle.UpdateChain();
+
+        if (this.puzzle.IsFinishedChain) {
+          this.puzzle.EndChain();
+        }
+      }
+    }
+
+#if _DEBUG
+    public void OnDebug()
+    {
+      this.status.OnDebug();
+    }
+#endif
+  }
+}
+
