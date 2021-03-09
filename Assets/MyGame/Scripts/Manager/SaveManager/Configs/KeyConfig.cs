855444de05e2cc847775199f2975cdb46263e27e
kazumichi kuroda <kazumichi96@gmail.com>
2021-03-09 17:09:55 +0900

    Added KeyConfig Object

---
+++ Assets/MyGame/Scripts/Manager/SaveManager/Configs/KeyConfig.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using MyGame.InputManagement;
+
+namespace MyGame.SaveManagement
+{
+  /// <summary>
+  /// キーボードとゲーム内のKeyTypeの紐づけを保持する
+  /// </summary>
+  [CreateAssetMenu(menuName = "MyGame/Create/KeyConfig")]
+  public class KeyConfig : ScriptableObject
+  {
+    //-------------------------------------------------------------------------
+    // クラス
+
+    [System.Serializable]
+    private class KeyMap
+    {
+      public KeyType Type;
+      public KeyCode Code = default;
+
+      public KeyMap(KeyType type)
+      {
+        Type = type;
+      }
+    }
+
+    //-------------------------------------------------------------------------
+    // メンバ変数
+
+    /// <summary>
+    /// KeyTypeとKeyCodeのマッピングデータ
+    /// </summary>
+    [SerializeField]
+    private List<KeyMap> keyMap = new List<KeyMap>();
+
+    //-------------------------------------------------------------------------
+    // ライフサイクル
+
+    /// <summary>
+    /// 右クリックメニューから生成されたときに一度動作する
+    /// </summary>
+    private void Awake()
+    {
+      MyEnum.ForEach<KeyType>((type) => { 
+        this.keyMap.Add(new KeyMap(type));
+      });
+    }
+
+    //-------------------------------------------------------------------------
+    // publicメソッド
+
+    /// <summary>
+    /// KeyMapの要素をコールバックを指定して取得する
+    /// </summary>
+    public void Gets(System.Action<KeyType, KeyCode> func)
+    {
+      this.keyMap.ForEach((map) => { 
+        func(map.Type, map.Code);
+      });
+    }
+
+#if UNITY_EDITOR
+    //---------------------------------------------------------------------------
+    // Inspector拡張
+
+    /// <summary>
+    /// KeyConfigのInspector拡張
+    /// </summary>
+    [CustomEditor(typeof(KeyConfig))]
+    public class KeyConfigEditor : ScriptableObjectInspectorEditorBase<KeyConfig>
+    {
+      public override void OnInspectorGUI()
+      {
+        MyEnum.ForEach<KeyType>((type, index) => 
+        {
+          using (new EditorGUILayout.HorizontalScope()) 
+          {
+            EditorGUILayout.LabelField($"{type}");
+            config.keyMap[index].Code = (KeyCode)EditorGUILayout.EnumPopup(config.keyMap[index].Code);
+          }
+        });
+
+        SaveButton();
+      }
+    }
+#endif
+  }
+}
