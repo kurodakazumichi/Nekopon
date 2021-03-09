68149d0a669fc2130d625927ca8053f10cd3bbcd
kazumichi kuroda <kazumichi96@gmail.com>
2021-03-10 07:58:20 +0900

    Added ゲージユニットのスクリプトを追加

---
+++ Assets/MyGame/Scripts/Unit/Versus/Gauge.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyGame.Unit.Versus
+{
+  public class Gauge : Unit<Gauge.State>
+  {
+    /// <summary>
+    /// 状態
+    /// </summary>
+    public enum State { }
+
+    //-------------------------------------------------------------------------
+    // メンバ変数
+
+    /// <summary>
+    /// ゲージのスプライトを表示するため
+    /// </summary>
+    private SpriteRenderer spriteRenderer = null;
+
+    //-------------------------------------------------------------------------
+    // Load, Unload
+
+    /// <summary>
+    /// カーソル用のSprite
+    /// </summary>
+    private static Sprite GaugeSprite = null;
+
+    public static void Load(System.Action pre, System.Action done)
+    {
+      var rm = ResourceManager.Instance;
+      rm.Load<Sprite>("Common.White.sprite", pre, done, (res) => { GaugeSprite = res; });
+    }
+
+    public static void Unload()
+    {
+      var rm = ResourceManager.Instance;
+      rm.Unload("Common.White.sprite");
+      GaugeSprite = null;
+    }
+
+    //-------------------------------------------------------------------------
+    // ライフサイクル
+
+    protected override void MyAwake()
+    {
+      this.spriteRenderer = this.gameObject.AddComponent<SpriteRenderer>();
+      this.spriteRenderer.sprite = GaugeSprite;
+    }
+
+  }
+}
