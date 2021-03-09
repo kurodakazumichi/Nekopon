35ed8be062c3bc9bd9246f59d80f7c10742a9701
kazumichi kuroda <kazumichi96@gmail.com>
2021-03-10 07:57:46 +0900

    #4 LocationにHP、APゲージの位置オブジェクトを追加

--- Assets/MyGame/Scripts/Manager/VersusManager/Location.cs
+++ Assets/MyGame/Scripts/Manager/VersusManager/Location.cs
@@ -7,14 +7,32 @@ namespace MyGame.VersusManagement
   /// <summary>
   /// 対戦画面内の各種ロケーション(座標)を格納したクラス
   /// </summary>
-  public struct Location
+  public class Location
   {
-    public Vector3 Paw { get; private set; }
+    /// <summary>
+    /// HPゲージの位置
+    /// </summary>
+    public Vector3 HpGuage { get; private set; } = Vector3.zero;
 
+    /// <summary>
+    /// APゲージの位置
+    /// </summary>
+    public Vector3 ApGuage { get; private set; } = Vector3.zero;
+
+    /// <summary>
+    /// 肉球の配置基準位置
+    /// </summary>
+    public Vector3 Paw { get; private set; } = Vector3.zero;
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
     public Location(string prefix, GameObject go)
     {
       Transform locations = go.transform;
       Paw = locations.Find($"{prefix}.Paw").position;
+      HpGuage = locations.Find($"{prefix}.Gauge.Hp").position;
+      ApGuage = locations.Find("${prefix}.Gauge.Ap").position;
     }
   }
 }
