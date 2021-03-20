0f62bfbf649e76c30d6da822ddb9686428ab937d
kazumichi kuroda <kazumichi96@gmail.com>
2021-03-20 21:50:31 +0900

    #9 IMoverを定義

---
+++ Assets/MyGame/Scripts/Unit/Mover/IMover.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace MyGame.Unit.Mover
+{
+  /// <summary>
+  /// MoverのInterface
+  /// </summary>
+  public interface IMover : IPoolable
+  {
+    /// <summary>
+    /// Transformが取得できる
+    /// </summary>
+    Transform CacheTransform { get; }
+
+    /// <summary>
+    /// SpriteRendrerが取得できる
+    /// </summary>
+    SpriteRenderer Renderer { get; }
+
+    /// <summary>
+    /// アルファ値のアクセッサ
+    /// </summary>
+    float Alpha { get; set; }
+
+    /// <summary>
+    /// 輝度のアクセッサ
+    /// </summary>
+    float Brightness { get; set; }
+  }
+}
