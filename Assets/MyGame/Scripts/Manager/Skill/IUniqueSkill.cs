c8a61f87628acdb41979f6d6683c56ec1c2557ae
kazumichi kuroda <kazumichi96@gmail.com>
2021-04-02 09:25:45 +0900

    #10 固有スキルのインターフェースを作成

---
+++ Assets/MyGame/Scripts/Manager/Skill/IUniqueSkill.cs
@@ -0,0 +1,20 @@
+﻿using MyGame.Unit.Versus;
+
+namespace MyGame
+{
+  /// <summary>
+  /// 固有スキルインターフェース
+  /// </summary>
+  public interface IUniqueSkill : IPoolable
+  {
+    /// <summary>
+    /// セットアップ可能
+    /// </summary>
+    void Setup(Define.App.UniqueSkill skillType, Define.App.Cat catType);
+
+    /// <summary>
+    /// 発動可能
+    /// </summary>
+    void Fire(Player owner, Player target);
+  }
+}
